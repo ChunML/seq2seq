123afb39bb0eac53fd7968f303a6af44379a7758
ChunML <ahzhongmusic@gmail.com>
2016-11-29 17:52:49 +0900

    Update batch data

--- seq2seq.py
+++ seq2seq.py
@@ -5,20 +5,26 @@ from keras.layers.recurrent import LSTM
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import text_to_word_sequence
 import numpy as np
+from nltk import FreqDist
 
 def load_data(filename):
     f = open(filename, 'r')
     data = f.read()
     f.close()
-    sentences = [text_to_word_sequence(sentence) for sentence in data.split('\n')[:10]]
-    vocab = list(set(np.hstack(sentences)))
-    
-    word_to_ix = {word:ix for ix, word in enumerate(vocab)}
-    ix_to_word = {ix:word for ix, word in enumerate(vocab)}
+    sentences = [text_to_word_sequence(sentence) for sentence in data.split('\n') if len(sentence) <= 200]
+    #vocab = list(set(np.hstack(sentences)))
+    dist = FreqDist(np.hstack(sentences))
+    vocab = dist.most_common(1000-1)
+    ix_to_word = [word[0] for word in vocab]
+    ix_to_word.append('UNK')
+    word_to_ix = {word:ix for ix, word in enumerate(ix_to_word)}
     for i, sentence in enumerate(sentences):
         for j, word in enumerate(sentence):
-            sentences[i][j] = word_to_ix[word]
-    return (sentences, len(vocab), word_to_ix, ix_to_word)
+            if word in word_to_ix:
+                sentences[i][j] = word_to_ix[word]
+            else:
+                sentences[i][j] = word_to_ix['UNK']
+    return (sentences, len(vocab)+1, word_to_ix, ix_to_word)
 
 def process_data(word_sentences, max_len, word_to_ix):
     sequences = np.zeros((len(word_sentences), max_len, len(word_to_ix)))
@@ -45,17 +51,20 @@ if __name__ == '__main__':
     y, y_vocab_len, y_word_to_ix, y_ix_to_word = load_data('europarl-v8.fi-en.fi')
 
     X_max_len = max([len(sentence) for sentence in X])
-    padded_X = pad_sequences(X, maxlen=X_max_len, dtype='uint8')
-
     y_max_len = max([len(sentence) for sentence in y])
-    padded_y = pad_sequences(y, maxlen=y_max_len, dtype='uint8')
+
+    print('[INFO] Compiling model...')
+    model = create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, 100, 1)
     
-    X_sequences = process_data(padded_X, X_max_len, X_word_to_ix)
-    print(X_sequences.shape)
+    for i in range(0, len(X), 1000):
+        padded_X = pad_sequences(X[i:i+1000], maxlen=X_max_len, dtype='uint8')
+    
+        padded_y = pad_sequences(y[i:i+1000], maxlen=y_max_len, dtype='uint8')
+    
+        X_sequences = process_data(padded_X, X_max_len, X_word_to_ix)
 
-    y_sequences = process_data(padded_y, y_max_len, y_word_to_ix)
-    print(y_sequences.shape)
+        y_sequences = process_data(padded_y, y_max_len, y_word_to_ix)
 
-    model = create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, 100, 3)
-    model.fit(X_sequences, y_sequences, batch_size=10, nb_epoch=100, validation_split=0.2, verbose=1)
-    print('Done')
+        print('[INFO] Training model... {}/{} samples'.format(i, len(X)))
+        model.fit(X_sequences, y_sequences, batch_size=10, nb_epoch=1, verbose=2)
+    
