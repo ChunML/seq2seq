5d7c87d627e572989560bf4da0f078190d61b70e
ChunML <ahzhong11491@gmail.com>
2016-12-09 00:53:34 +0900

    Fix zero padding

--- seq2seq.py
+++ seq2seq.py
@@ -4,12 +4,13 @@ from keras.layers import Activation, TimeDistributed, Dense, RepeatVector, recur
 from keras.layers.recurrent import LSTM
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import text_to_word_sequence
+from keras.optimizers import Adam
 import numpy as np
 from nltk import FreqDist
 import argparse
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-max_len', type=int, default=300)
+ap.add_argument('-max_len', type=int, default=200)
 ap.add_argument('-vocab_size', type=int, default=20000)
 ap.add_argument('-batch_size', type=int, default=100)
 ap.add_argument('-layer_num', type=int, default=3)
@@ -34,8 +35,8 @@ def load_data(source, dist, max_len, vocab_size):
     y_data = f.read()
     f.close()
 
-    X = [text_to_word_sequence(x)[::-1] for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) <= max_len and len(y) <= max_len]
-    y = [text_to_word_sequence(y) for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) <= max_len and len(y) <= max_len]
+    X = [text_to_word_sequence(x)[::-1] for x, y in zip(X_data.split('\n')[:1000], y_data.split('\n')[:1000]) if len(x) <= max_len and len(y) <= max_len]
+    y = [text_to_word_sequence(y) for x, y in zip(X_data.split('\n')[:1000], y_data.split('\n')[:1000]) if len(x) <= max_len and len(y) <= max_len]
 
     dist = FreqDist(np.hstack(X))
     X_vocab = dist.most_common(vocab_size-1)
@@ -43,7 +44,8 @@ def load_data(source, dist, max_len, vocab_size):
     y_vocab = dist.most_common(vocab_size-1)
 
     X_ix_to_word = [word[0] for word in X_vocab]
-    X_ix_to_word.insert(0, 'UNK')
+    X_ix_to_word.insert(0, 'ZERO')
+    X_ix_to_word.append('UNK')
     X_word_to_ix = {word:ix for ix, word in enumerate(X_ix_to_word)}
     for i, sentence in enumerate(X):
         for j, word in enumerate(sentence):
@@ -53,6 +55,7 @@ def load_data(source, dist, max_len, vocab_size):
                 X[i][j] = X_word_to_ix['UNK']
 
     y_ix_to_word = [word[0] for word in y_vocab]
+    y_ix_to_word.insert(0, 'ZERO')
     y_ix_to_word.append('UNK')
     y_word_to_ix = {word:ix for ix, word in enumerate(y_ix_to_word)}
     for i, sentence in enumerate(y):
@@ -61,7 +64,7 @@ def load_data(source, dist, max_len, vocab_size):
                 y[i][j] = y_word_to_ix[word]
             else:
                 y[i][j] = y_word_to_ix['UNK']
-    return (X, len(X_vocab)+1, X_word_to_ix, X_ix_to_word, y, len(y_vocab)+1, y_word_to_ix, y_ix_to_word)
+    return (X, len(X_vocab)+2, X_word_to_ix, X_ix_to_word, y, len(y_vocab)+2, y_word_to_ix, y_ix_to_word)
 
 
 def process_data(word_sentences, max_len, word_to_ix):
@@ -81,6 +84,7 @@ def create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, hidden_size, nu
         model.add(LSTM(hidden_size, return_sequences=True))
     model.add(TimeDistributed(Dense(y_vocab_len)))
     model.add(Activation('softmax'))
+    #adam = Adam(lr=0.001)
     model.compile(loss='categorical_crossentropy',
             optimizer='rmsprop',
             metrics=['accuracy'])
@@ -107,7 +111,7 @@ if __name__ == '__main__':
         y = y[indices]
         for i in range(0, len(X), 1280):
             if i + 1280 >= len(X):
-                i_end = len(X) - 1
+                i_end = len(X)
             else:
                 i_end = i + 1280
             y_sequences = process_data(y[i:i_end], y_max_len, y_word_to_ix)
