4d40bbbdc53bd7df24a366875230a7f67ef1ee10
ChunML <ahzhongmusic@gmail.com>
2016-12-20 13:35:56 +0900

    Add implementation for test, split source file into main file and utils file

--- seq2seq.py
+++ seq2seq.py
@@ -1,13 +1,10 @@
 from __future__ import print_function
-from keras.models import Sequential
-from keras.layers import Activation, TimeDistributed, Dense, RepeatVector, recurrent, Embedding
-from keras.layers.recurrent import LSTM
 from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import text_to_word_sequence
-from keras.optimizers import Adam, RMSprop
 import numpy as np
-from nltk import FreqDist
+import sys
+
 import argparse
+from seq2seq_utils import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-max_len', type=int, default=200)
@@ -16,6 +13,7 @@ ap.add_argument('-batch_size', type=int, default=100)
 ap.add_argument('-layer_num', type=int, default=3)
 ap.add_argument('-hidden_dim', type=int, default=1000)
 ap.add_argument('-nb_epoch', type=int, default=20)
+ap.add_argument('-mode', default='train')
 args = vars(ap.parse_args())
 
 MAX_LEN = args['max_len']
@@ -24,75 +22,7 @@ BATCH_SIZE = args['batch_size']
 LAYER_NUM = args['layer_num']
 HIDDEN_DIM = args['hidden_dim']
 NB_EPOCH = args['nb_epoch']
-
-def load_data(source, dist, max_len, vocab_size):
-    f = open(source, 'r')
-    X_data = f.read()
-    f.close()
-    f = open(dist, 'r')
-    y_data = f.read()
-    f.close()
-
-    X = [text_to_word_sequence(x)[::-1] for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) > 0 and len(y) > 0 and len(x) <= max_len and len(y) <= max_len]
-    y = [text_to_word_sequence(y) for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) > 0 and len(y) > 0 and len(x) <= max_len and len(y) <= max_len]
-
-    print(len(X_data.split('\n')))
-    print(len(y_data.split('\n')))
-    print(len(X))
-    print(len(y))
-    dist = FreqDist(np.hstack(X))
-    X_vocab = dist.most_common(vocab_size-1)
-    dist = FreqDist(np.hstack(y))
-    y_vocab = dist.most_common(vocab_size-1)
-
-    X_ix_to_word = [word[0] for word in X_vocab]
-    X_ix_to_word.insert(0, 'ZERO')
-    X_ix_to_word.append('UNK')
-    X_word_to_ix = {word:ix for ix, word in enumerate(X_ix_to_word)}
-    for i, sentence in enumerate(X):
-        for j, word in enumerate(sentence):
-            if word in X_word_to_ix:
-                X[i][j] = X_word_to_ix[word]
-            else:
-                X[i][j] = X_word_to_ix['UNK']
-
-    y_ix_to_word = [word[0] for word in y_vocab]
-    y_ix_to_word.insert(0, 'ZERO')
-    y_ix_to_word.append('UNK')
-    y_word_to_ix = {word:ix for ix, word in enumerate(y_ix_to_word)}
-    for i, sentence in enumerate(y):
-        for j, word in enumerate(sentence):
-            if word in y_word_to_ix:
-                y[i][j] = y_word_to_ix[word]
-            else:
-                y[i][j] = y_word_to_ix['UNK']
-    return (X, len(X_vocab)+2, X_word_to_ix, X_ix_to_word, y, len(y_vocab)+2, y_word_to_ix, y_ix_to_word)
-
-
-def process_data(word_sentences, max_len, word_to_ix):
-    sequences = np.zeros((len(word_sentences), max_len, len(word_to_ix)))
-    for i, sentence in enumerate(word_sentences):
-        for j, word in enumerate(sentence):
-            sequences[i, j, word] = 1.
-    return sequences
-
-def create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, hidden_size, num_layers):
-    model = Sequential()
-    model.add(Embedding(X_vocab_len, 1000, input_length=X_max_len, mask_zero=True))
-    model.add(LSTM(hidden_size))
-    #model.add(LSTM(hidden_size, input_shape=(X_max_len, X_vocab_len)))
-    model.add(RepeatVector(y_max_len))
-    for _ in range(num_layers):
-        model.add(LSTM(hidden_size, return_sequences=True))
-    model.add(TimeDistributed(Dense(y_vocab_len)))
-    model.add(Activation('softmax'))
-    #adam = Adam(lr=0.001)
-    opt = RMSprop(lr=0.0003)
-    model.compile(loss='categorical_crossentropy',
-            optimizer=opt,
-            metrics=['accuracy'])
-    model.load_weights('checkpoint_epoch_10.hdf5')
-    return model
+MODE = args['mode']
 
 if __name__ == '__main__':
 
@@ -108,19 +38,36 @@ if __name__ == '__main__':
     print('[INFO] Compiling model...')
     model = create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, HIDDEN_DIM, LAYER_NUM)
 
-    i_end = 0
-    for k in range(11, NB_EPOCH+1):
-        indices = np.arange(len(X))
-        np.random.shuffle(indices)
-        X = X[indices]
-        y = y[indices]
-        for i in range(0, len(X), 1000):
-            if i + 1000 >= len(X):
-                i_end = len(X)
-            else:
-                i_end = i + 1000
-            y_sequences = process_data(y[i:i_end], y_max_len, y_word_to_ix)
+    saved_weights = find_checkpoint_file('.')
 
-            print('[INFO] Training model: epoch {}th {}/{} samples'.format(k, i, len(X)))
-            model.fit(X[i:i_end], y_sequences, batch_size=BATCH_SIZE, nb_epoch=1, verbose=2)
-        model.save_weights('checkpoint_epoch_{}.hdf5'.format(k))
+    if MODE == 'train':
+        k_start = 0
+        if len(save_weights) != 0:
+            epoch = saved_weights[save_weights.rfind('_'+1):save_weights.rfind('.')]
+            model.load_weights(save_weights)
+            k_start = epoch + 1
+
+        i_end = 0
+        for k in range(k_start, NB_EPOCH+1):
+            indices = np.arange(len(X))
+            np.random.shuffle(indices)
+            X = X[indices]
+            y = y[indices]
+            for i in range(0, len(X), 1000):
+                if i + 1000 >= len(X):
+                    i_end = len(X)
+                else:
+                    i_end = i + 1000
+                y_sequences = process_data(y[i:i_end], y_max_len, y_word_to_ix)
+
+                print('[INFO] Training model: epoch {}th {}/{} samples'.format(k, i, len(X)))
+                model.fit(X[i:i_end], y_sequences, batch_size=BATCH_SIZE, nb_epoch=1, verbose=2)
+            model.save_weights('checkpoint_epoch_{}.hdf5'.format(k))
+    else:
+        if len(save_weights) == 0:
+            print("The network hasn't been trained! Program will exit...")
+            sys.exit()
+        else:
+            X_test = load_test_data('test', X_word_to_ix)
+            model.load_weights(save_weights)
+            y_pred = np.argmax(model.predict(X_test)[0], axis=1)
---
+++ seq2seq_utils.py
@@ -0,0 +1,95 @@
+from keras.preprocessing.text import text_to_word_sequence
+from keras.models import Sequential
+from keras.layers import Activation, TimeDistributed, Dense, RepeatVector, recurrent, Embedding
+from keras.layers.recurrent import LSTM
+from keras.optimizers import Adam, RMSprop
+from nltk import FreqDist
+import numpy as np
+import os
+import datetime
+
+def load_data(source, dist, max_len, vocab_size):
+    f = open(source, 'r')
+    X_data = f.read()
+    f.close()
+    f = open(dist, 'r')
+    y_data = f.read()
+    f.close()
+
+    X = [text_to_word_sequence(x)[::-1] for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) > 0 and len(y) > 0 and len(x) <= max_len and len(y) <= max_len]
+    y = [text_to_word_sequence(y) for x, y in zip(X_data.split('\n'), y_data.split('\n')) if len(x) > 0 and len(y) > 0 and len(x) <= max_len and len(y) <= max_len]
+
+    print(len(X_data.split('\n')))
+    print(len(y_data.split('\n')))
+    print(len(X))
+    print(len(y))
+    dist = FreqDist(np.hstack(X))
+    X_vocab = dist.most_common(vocab_size-1)
+    dist = FreqDist(np.hstack(y))
+    y_vocab = dist.most_common(vocab_size-1)
+
+    X_ix_to_word = [word[0] for word in X_vocab]
+    X_ix_to_word.insert(0, 'ZERO')
+    X_ix_to_word.append('UNK')
+    X_word_to_ix = {word:ix for ix, word in enumerate(X_ix_to_word)}
+    for i, sentence in enumerate(X):
+        for j, word in enumerate(sentence):
+            if word in X_word_to_ix:
+                X[i][j] = X_word_to_ix[word]
+            else:
+                X[i][j] = X_word_to_ix['UNK']
+
+    y_ix_to_word = [word[0] for word in y_vocab]
+    y_ix_to_word.insert(0, 'ZERO')
+    y_ix_to_word.append('UNK')
+    y_word_to_ix = {word:ix for ix, word in enumerate(y_ix_to_word)}
+    for i, sentence in enumerate(y):
+        for j, word in enumerate(sentence):
+            if word in y_word_to_ix:
+                y[i][j] = y_word_to_ix[word]
+            else:
+                y[i][j] = y_word_to_ix['UNK']
+    return (X, len(X_vocab)+2, X_word_to_ix, X_ix_to_word, y, len(y_vocab)+2, y_word_to_ix, y_ix_to_word)
+
+def load_test_data(source, X_word_to_ix):
+    f = open(source, 'r')
+    X_data = f.read()
+    f.close()
+
+    X = [text_to_word_sequence(x)[::-1] for x, y in X_data.split('\n') if len(x) > 0 and len(x) <= max_len]
+    for i, sentence in enumerate(X):
+        for j, word in enumerate(sentence):
+            if word in X_word_to_ix:
+                X[i][j] = X_word_to_ix[word]
+            else:
+                X[i][j] = X_word_to_ix['UNK']
+    return X
+
+def create_model(X_vocab_len, X_max_len, y_vocab_len, y_max_len, hidden_size, num_layers):
+    model = Sequential()
+    model.add(Embedding(X_vocab_len, 1000, input_length=X_max_len, mask_zero=True))
+    model.add(LSTM(hidden_size))
+    model.add(RepeatVector(y_max_len))
+    for _ in range(num_layers):
+        model.add(LSTM(hidden_size, return_sequences=True))
+    model.add(TimeDistributed(Dense(y_vocab_len)))
+    model.add(Activation('softmax'))
+    opt = RMSprop(lr=0.0003)
+    model.compile(loss='categorical_crossentropy',
+            optimizer=opt,
+            metrics=['accuracy'])
+    return model
+
+def process_data(word_sentences, max_len, word_to_ix):
+    sequences = np.zeros((len(word_sentences), max_len, len(word_to_ix)))
+    for i, sentence in enumerate(word_sentences):
+        for j, word in enumerate(sentence):
+            sequences[i, j, word] = 1.
+    return sequences
+
+def find_checkpoint_file():
+    checkpoint_file = [f for f in os.listdir(folder) if 'checkpoint' in f]
+    if len(checkpoint_file) == 0:
+        return []
+    modified_time = [os.path.getmtime(f) for f in checkpoint_file]
+    return checkpoint_file[np.argmax(modified_time)]
